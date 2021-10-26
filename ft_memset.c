1020ed6a2f89dc627ea1a92c2f796f43eb2ea214
0xNino <86327707+0xNino@users.noreply.github.com>
2021-10-26 15:13:16 +0200

    memset

---
+++ ft_memset.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngenoud <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 13:46:11 by ngenoud           #+#    #+#             */
+/*   Updated: 2021/10/26 13:46:13 by ngenoud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	void	*ptr;
+
+	ptr = b;
+	while (len--)
+		*(unsigned char *) ptr++ = (unsigned char) c;
+	return (b);
+}
+/*
+int	main(void)
+{
+	char	arr[10];
+
+	strcpy(arr, "123456789");
+	printf("%s\n", arr);
+	ft_memset(arr, '$', 4);
+	printf("%s\n", arr);
+}
+*/
