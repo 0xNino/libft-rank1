de095a351cb6a8f776e6dc4a07e0657884f27989
0xNino <86327707+0xNino@users.noreply.github.com>
2021-10-26 14:13:47 +0200

    finish isprint memset strlen

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,31 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngenoud <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/26 11:32:34 by ngenoud           #+#    #+#             */
-/*   Updated: 2021/10/26 11:32:37 by ngenoud          ###   ########.fr       */
+/*   Created: 2021/10/26 13:39:28 by ngenoud           #+#    #+#             */
+/*   Updated: 2021/10/26 13:39:30 by ngenoud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-
-typedef struct s_list {
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}	t_list;
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	return (32 <= c && c <= 127);
+}
