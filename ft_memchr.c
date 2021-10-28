ea288f398c2f11499da28cc569465879d154dc14
0xNino <86327707+0xNino@users.noreply.github.com>
2021-10-28 22:34:36 +0200

    start memchr

---
+++ ft_memchr.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngenoud <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 22:00:11 by ngenoud           #+#    #+#             */
+/*   Updated: 2021/10/28 22:00:12 by ngenoud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	while ((unsigned const char)&s != (unsigned char)c && n--)
+		if (!s++)
+			return (NULL);
+	return ((void *)s);
+}
