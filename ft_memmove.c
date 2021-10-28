095ee8189138d33bc8d8fc5e255f78dd2381e4af
0xNino <86327707+0xNino@users.noreply.github.com>
2021-10-28 17:53:51 +0200

    fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,24 +14,68 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char		*pd;
-	const unsigned char	*ps;
+	unsigned int	i;
 
-	pd = dst;
-	ps = src;
-	if (!pd || !ps)
-		return (NULL);
-	if (pd < ps)
+	i = 0;
+	if ((unsigned char *) src < (unsigned char *) dst)
 	{
-		pd += len;
 		while (len--)
-			*--pd = *--ps;
+			((unsigned char *) dst)[len] = ((unsigned char *) src)[len];
 	}
 	else
-		while (len--)
-			*pd++ = *ps++;
+	{
+		while (i < len)
+		{
+			((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+			i++;
+		}
+	}
 	return (dst);
 }
+/*
+int	main(int argc, const char *argv[])
+{
+	char	src[] = "lorem ipsum dolor sit amet";
+	char	*dest;
+	int		arg;
+
+	dest = src + 1;
+	alarm(5);
+	if (argc == 1)
+		return (0);
+	else if ((arg = atoi(argv[1])) == 1)
+	{
+		if (dest != ft_memmove(dest, "consectetur", 5))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+	}
+	else if (arg == 2)
+	{
+		if (dest != ft_memmove(dest, "con\0sec\0\0te\0tur", 10))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+	}
+	else if (arg == 3)
+	{
+		if (dest != ft_memmove(dest, src, 8))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+	}
+	else if (arg == 4)
+	{
+		if (src != ft_memmove(src, dest, 8))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+	}
+	else if (arg == 5)
+	{
+		if (src != ft_memmove(src, dest, 0))
+			write(1, "dest's adress was not returned\n", 31);
+		write(1, dest, 22);
+	}
+	return (0);
+}
+*/
 /*
 int	main(void)
 {
--- libft.h
+++ libft.h
@@ -29,14 +29,17 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(const char *str);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strdup(const char *s1);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+char	*ft_strdup(const char *s1);
 
 #endif
