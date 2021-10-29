f872a773a341ec6d3b9f9043f22cc4a1fb9f47aa
0xNino <86327707+0xNino@users.noreply.github.com>
2021-10-29 16:02:05 +0200

    fix memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -17,7 +17,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((*(unsigned char *)s1 || *(unsigned char *)s2) && i++ < n)
+	while (i++ < n)
 	{
 		if (*(unsigned char *)s1 != *(unsigned char *)s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
@@ -26,3 +26,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
+/*
+int	main(void)
+{
+	char	s2[] = {0, 0, 127, 0};
+	char    s3[] = {0, 0, 42, 0};
+	printf("%d\n", ft_memcmp(s2, s3, 4));
+	printf("%d\n", memcmp(s2, s3, 4));
+}
+*/
