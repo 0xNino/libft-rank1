2f30934c37c25a19aee1bba87b2839e001882b79
0xNino <86327707+0xNino@users.noreply.github.com>
2021-10-29 15:50:18 +0200

    fix ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -19,20 +19,17 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	src_len;
 
 	i = 0;
-	dst_len = 0;
-	src_len = 0;
-	while (dst[dst_len])
-		dst_len++;
-	while (src[src_len])
-		src_len++;
-	if (dstsize < dst_len)
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize < dst_len + 1)
 		return (src_len + dstsize);
 	while (i < (dstsize - dst_len - 1) && src[i])
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
-	dst[dst_len] = '\0';
+	if (dst_len != dstsize)
+		dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
 }
 /*
@@ -49,19 +46,29 @@ void	ft_print_result(int n)
 int		main(int argc, const char *argv[])
 {
 	char	*dest;
+	char	*dest2;
 	int		arg;
 
 	alarm(5);
 	if (!(dest = (char *)malloc(sizeof(*dest) * 15)) || argc == 1)
 		return (0);
+	if (!(dest2 = (char *)malloc(sizeof(*dest2) * 15)) || argc == 1)
+		return (0);
 	memset(dest, 0, 15);
 	memset(dest, 'r', 6);
+	memset(dest2, 0, 15);
+	memset(dest2, 'r', 6);
 	if ((arg = atoi(argv[1])) == 1)
 	{
 		dest[11] = 'a';
+		dest2[11] = 'a';
 		ft_print_result(ft_strlcat(dest, "lorem", 15));
 		write(1, "\n", 1);
 		write(1, dest, 15);
+		write(1, "\n", 1);
+		ft_print_result(strlcat(dest2, "lorem", 15));
+		write(1, "\n", 1);
+		write(1, dest2, 15);
 	}
 	else if (arg == 2)
 	{
@@ -111,6 +118,10 @@ int		main(int argc, const char *argv[])
 		ft_print_result(ft_strlcat(dest, "lorem ipsum dolor sit amet", 6));
 		write(1, "\n", 1);
 		write(1, dest, 15);
+		write(1, "\n", 1);
+		ft_print_result(strlcat(dest2, "lorem", 15));
+		write(1, "\n", 1);
+		write(1, dest2, 15);
 	}
 	else if (arg == 9)
 	{
