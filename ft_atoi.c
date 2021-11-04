46bb9beb7e754c9cb16fd6ba043dcd5574dca84c
0xNino <86327707+0xNino@users.noreply.github.com>
2021-11-04 22:44:23 +0100

    fix atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngenoud <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 18:57:40 by ngenoud           #+#    #+#             */
-/*   Updated: 2021/10/28 18:57:44 by ngenoud          ###   ########.fr       */
+/*   Updated: 2021/11/04 22:41:02 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,26 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	neg;
-	int	sign;
+	long	nbr;
+	int		minus;
 
-	i = 0;
-	neg = 0;
-	sign = 0;
-	while (*str && (ft_isspace(*str)))
+	nbr = 0;
+	minus = 0;
+	while ((9 <= *str && *str <= 13) || *str == 32)
 		str++;
-	while (*str && (*str == '+' || *str == '-'))
+	if (*str == '+' || *str == '-')
+		if (*str++ == '-')
+			minus = 1;
+	while (ft_isdigit(*str))
 	{
-		if (*str == '-')
-			neg++;
-		sign++;
+		if ((nbr > (FT_LONG_MAX - *str + '0') / 10) && minus)
+			return (0);
+		if ((nbr > (FT_LONG_MAX - *str + '0') / 10) && !minus)
+			return (-1);
+		nbr = 10 * nbr + (*str - '0');
 		str++;
 	}
-	while ('0' <= *str && *str <= '9')
-		i = i * 10 + (int) *str++ - '0';
-	if (sign > 1)
-		return (0);
-	if (neg)
-		return (i * -1);
-	else
-		return (i);
+	if (minus)
+		return ((int)-nbr);
+	return ((int)nbr);
 }
