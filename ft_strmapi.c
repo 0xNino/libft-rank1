25b6968e528f7072f065e80fdbfe957dda7df7ba
0xNino <86327707+0xNino@users.noreply.github.com>
2021-11-05 14:29:00 +0100

    finish strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,13 +6,27 @@
 /*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 23:09:33 by 0xNino            #+#    #+#             */
-/*   Updated: 2021/11/04 23:14:27 by 0xNino           ###   ########.fr       */
+/*   Updated: 2021/11/05 14:19:25 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	str = ft_strdup(s);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (*f)(i, str[i]);
+		i++;
+	}
+	return (str);
 }
-*/
