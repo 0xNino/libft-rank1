c2326ae7bc38a519fe22551ec8eb45bd1c688a95
0xNino <86327707+0xNino@users.noreply.github.com>
2021-11-05 15:39:36 +0100

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngenoud <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:29:21 by ngenoud           #+#    #+#             */
-/*   Updated: 2021/11/01 16:29:22 by ngenoud          ###   ########.fr       */
+/*   Updated: 2021/11/05 15:36:48 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	if (start >= ft_strlen(s))
-		return (calloc(1, sizeof(char)));
-	sub = (char *)malloc(sizeof(char) * (len + 1));
-	if (!sub)
-		return (NULL);
+	{
+		sub = calloc(1, sizeof(char));
+		if (!sub)
+			return (NULL);
+		return (sub);
+	}
+	if (len > ft_strlen(s))
+	{
+		sub = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+		if (!sub)
+			return (NULL);
+	}
+	else
+	{
+		sub = (char *)malloc(sizeof(char) * (len + 1));
+		if (!sub)
+			return (NULL);
+	}
 	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
