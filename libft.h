1514b90ef39089dd117e8cc1e0f70b7582c62fd2
0xNino <86327707+0xNino@users.noreply.github.com>
2021-11-05 15:13:46 +0100

    finish putchar_fd putendl_fd putnbr_fd putstr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 11:32:34 by ngenoud           #+#    #+#             */
-/*   Updated: 2021/11/05 14:40:39 by 0xNino           ###   ########.fr       */
+/*   Updated: 2021/11/05 15:12:14 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,9 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
