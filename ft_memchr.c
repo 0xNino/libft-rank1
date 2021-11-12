c82819949d14f5b60a5c62b80c593e7bcbb3df7d
0xNino <86327707+0xNino@users.noreply.github.com>
2021-11-12 19:11:12 +0100

    fix memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: 0xNino <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 22:00:11 by ngenoud           #+#    #+#             */
-/*   Updated: 2021/11/12 18:17:11 by 0xNino           ###   ########.fr       */
+/*   Updated: 2021/11/12 19:10:46 by 0xNino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while (*(unsigned const char *)s != (unsigned char)c && n--)
+	while (n--)
 	{
-		if (!n)
-			return (NULL);
+		if (*(unsigned const char *)s == (unsigned char)c)
+			return ((void *)s);
 		s++;
 	}
-	if (n == 0)
-		return (NULL);
-	return ((void *)s);
+	return (NULL);
 }
